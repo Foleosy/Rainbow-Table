341ef70f62a8d04d6ab94fa2f29ac329cce1a626
Foleosy <mywong@outlook.sg>
2016-09-23 23:34:26 +0800

    Created and Updated Header file

--- RainbowTable/RainbowTable/Generate.cpp
+++ RainbowTable/RainbowTable/Generate.cpp
@@ -9,20 +9,22 @@ to run
 
 ------------*/
 
-#include <iostream>
-#include <iomanip>
-#include "sha1.h"
+#include "Generate.h"
+
+Generate::Generate() {}
+
+Generate::~Generate() {}
 
 using namespace std;
-
 //-----   H A S H      ------------------------------------------------
 //   getrandom (m) :     generate a random word. 
 //---------------------------------------------------------------------
-int getrandom(unsigned char  m[3])
+int Generate::getrandom(unsigned char  m[3], int seed)
 {
-	m[0] = (unsigned char)(random() % 256);
-	m[1] = (unsigned char)(random() % 256);
-	m[2] = (unsigned char)(random() % 256);
+	srand(seed);
+	m[0] = (unsigned char)(rand() % 256);
+	m[1] = (unsigned char)(rand() % 256);
+	m[2] = (unsigned char)(rand() % 256);
 
 	return(0);
 }
@@ -30,7 +32,7 @@ int getrandom(unsigned char  m[3])
 //-----   H A S H      ------------------------------------------------
 //   Hash (m, d) :      compute the hash of m. The digest is stored in d.
 //---------------------------------------------------------------------
-int Hash(unsigned char m[3], unsigned int d[5])
+int Generate::Hash(unsigned char m[3], unsigned int d[5])
 {
 	SHA1 sha;
 
@@ -38,29 +40,31 @@ int Hash(unsigned char m[3], unsigned int d[5])
 	sha.Result(d);
 	return(0);
 }
-
-int main(int argc, char*argv[])
+/*
+void initialiseGenerator(int a, int b)
 {
+	
+	Generate generate;
 	SHA1          sha;
 	unsigned int  d[5];   // 32 x 5 = 160 bits  digest
 	unsigned char m[3];   // 24-bit word
 
-	int seed = atoi(argv[1]);
-	int number = atoi(argv[2]);
-	srandom(seed);
+	int seed = a;
+	int number = b;
+	srand(seed);
 
 	cout.setf(ios::hex, ios::basefield);    // format the output to be hex
 	cout.setf(ios::uppercase);
-
-	for (int i = 0; i<number; i++)
+	
+	for (int i = 0; i < number; i++)
 	{
-		getrandom(m); Hash(m, d);
+		generate.getrandom(m); generate.Hash(m, d);
 		cout << "  " << setw(8) << setfill('0') << d[0]          // setw(8) set the width to be 8.
 			<< "  " << setw(8) << d[1]
 			<< "  " << setw(8) << d[2]
 			<< "  " << setw(8) << d[3]
 			<< "  " << setw(8) << d[4] << endl;
 	}
+}*/
 
-}
 
--- RainbowTable/RainbowTable/Generate.h
+++ RainbowTable/RainbowTable/Generate.h
@@ -1,8 +1,21 @@
 #pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "sha1.h"
+
 class Generate
 {
 public:
 	Generate();
 	~Generate();
-};
 
+	unsigned int  d[5];   // 32 x 5 = 160 bits  digest
+	unsigned char m[3];   // 24-bit word
+
+	int getrandom(unsigned char m[3], int seed);
+	int Hash(unsigned char m[3], unsigned int d[5]);
+};
